e8c4639ec5a69d84fc506bf432e16922d1ad157d
LevarGoldun <>
2024-10-13 22:52:38 +0200

    Komunikace pomoci soketu, konecny cyklus

--- test_8_call_python_from_matlab/matlab_client.m
+++ test_8_call_python_from_matlab/matlab_client.m
@@ -3,10 +3,10 @@ tcpClient = tcpclient('localhost', 65432, 'ConnectTimeout', 10);
 
 while true
     % Чтение данных, отправленных с MuJoCo
-    rawData = read(tcpClient,1, "char");
+    rawData = read(tcpClient);
     
     % Преобразование строки в числовой массив
-    data = str2double(strsplit(char(rawData), ','));
+    data = native2unicode(rawData, "UTF-8");
 
     % Вывод данных
     disp("Данные из MuJoCo:");
@@ -17,8 +17,8 @@ while true
     % writeline(tcpClient, confirmationMessage);
 
     % Задержка
-    pause(0.01);
+    pause(1);
 end
 
 % Закрытие соединения
-clear tcpClient
+clear tcpClient;
--- test_8_call_python_from_matlab/python_server.py
+++ test_8_call_python_from_matlab/python_server.py
@@ -4,14 +4,10 @@ import mujoco.viewer
 
 import socket  # ChatGPT
 
-# Nastaveni soketu
-HOST = 'localhost'
-PORT = 65432
-
 # Vytvoreni soketu
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_socket.bind((HOST, PORT))
-server_socket.listen()
+server_socket.bind(('localhost', 65432))
+server_socket.listen(1)
 
 print("Cekame spojeni s MATLAB...")
 client_socket, addr = server_socket.accept()
@@ -22,7 +18,7 @@ xml_path = 'model_quadcopter_v1.xml'
 model = mj.MjModel.from_xml_path(xml_path)  # MuJoCo model
 data = mj.MjData(model)  # MuJoCo data
 
-
+last_time = time.time()
 with mujoco.viewer.launch_passive(model, data) as viewer:
     # Close the viewer automatically after 30 wall-seconds.
     start = time.time()
@@ -42,10 +38,14 @@ with mujoco.viewer.launch_passive(model, data) as viewer:
             else:
                 data.ctrl = [5, 5, 5, 5]
 
-        # Отправка данных в MATLAB
-        message = ','.join(map(str, data.ctrl.flatten()))
-        client_socket.sendall(message.encode('utf-8'))  # posilame do Matlab
-        #confirmation = client_socket.recv(1024).decode('utf-8')  # cekame odpoved od Matlab
+            # Отправка данных в MATLAB
+            if step_start - last_time > 0.1:
+                last_time = step_start
+
+                print('данные', data.ctrl)
+                message = ','.join(map(str, data.ctrl.flatten()))
+                client_socket.sendall(message.encode('utf-8'))  # posilame do Matlab
+                #confirmation = client_socket.recv(1024).decode('utf-8')  # cekame odpoved od Matlab
 
         # Pick up changes to the physics state, apply perturbations, update options from GUI.
         viewer.sync()
