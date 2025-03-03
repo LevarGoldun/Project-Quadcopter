0125623ccb371032686edd9b752f2422b38133d3
LevarGoldun <>
2025-03-03 20:40:31 +0100

    State feedback design for model with new angles, poles selection

---
+++ test_10_mat_equ_with_load/odvozeni_stavova_regulace_OTHER_ANGLES.m
@@ -0,0 +1,115 @@
+% 03.03.2025
+% Navrch stavove zpetne vazby pro soustavu kvadrokoptery
+% se zavazim S JINYM ODECNEM UHLU ZAVAZI 
+% (!!! odvozeni_nelin_rovnic_INERTIA_LOAD_OTHER_ANGLES.mlx)
+
+% Asi tady bude 2 zpusoby-> pole placement a LQR (tenhle jeste neumim)
+
+%% State Feedback 
+% Rozsirena stavova regulace s pozorovatelem (Luemberguv)
+
+% Analyza stability vychoziho systemu
+% Matice Ac a Bc z odvozeni_nelin_rovnic_INERTIA_LOAD_OTHER_ANGLES.mlx, 
+% POKUD SE ZMENI, NUTNE RUCNE ZMENIT TADY:
+syms M m g d Ip Ixx Iyy Izz
+
+Ac = [
+0, 0, 0,                 0,                  0, 0,                  0,                  0, 1, 0, 0, 0, 0, 0, 0, 0;
+0, 0, 0,                 0,                  0, 0,                  0,                  0, 0, 1, 0, 0, 0, 0, 0, 0;
+0, 0, 0,                 0,                  0, 0,                  0,                  0, 0, 0, 1, 0, 0, 0, 0, 0;
+0, 0, 0,                 0,                  0, 0,                  0,                  0, 0, 0, 0, 1, 0, 0, 0, 0;
+0, 0, 0,                 0,                  0, 0,                  0,                  0, 0, 0, 0, 0, 1, 0, 0, 0;
+0, 0, 0,                 0,                  0, 0,                  0,                  0, 0, 0, 0, 0, 0, 1, 0, 0;
+0, 0, 0,                 0,                  0, 0,                  0,                  0, 0, 0, 0, 0, 0, 0, 1, 0;
+0, 0, 0,                 0,                  0, 0,                  0,                  0, 0, 0, 0, 0, 0, 0, 0, 1;
+0, 0, 0,                 0,      (g*(M + m))/M, 0,            (g*m)/M,                  0, 0, 0, 0, 0, 0, 0, 0, 0;
+0, 0, 0,    -(g*(M + m))/M,                  0, 0,                  0,            (g*m)/M, 0, 0, 0, 0, 0, 0, 0, 0;
+0, 0, 0,                 0,                  0, 0,                  0,                  0, 0, 0, 0, 0, 0, 0, 0, 0;
+0, 0, 0,                 0,                  0, 0,                  0,                  0, 0, 0, 0, 0, 0, 0, 0, 0;
+0, 0, 0,                 0,                  0, 0,                  0,                  0, 0, 0, 0, 0, 0, 0, 0, 0;
+0, 0, 0,                 0,                  0, 0,                  0,                  0, 0, 0, 0, 0, 0, 0, 0, 0;
+0, 0, 0,                 0, -(g*(M + m))/(M*d), 0, -(g*(M + m))/(M*d),                  0, 0, 0, 0, 0, 0, 0, 0, 0;
+0, 0, 0, (g*(M + m))/(M*d),                  0, 0,                  0, -(g*(M + m))/(M*d), 0, 0, 0, 0, 0, 0, 0, 0];
+
+Bc = [
+        0,     0,     0,     0;
+        0,     0,     0,     0;
+        0,     0,     0,     0;
+        0,     0,     0,     0;
+        0,     0,     0,     0;
+        0,     0,     0,     0;
+        0,     0,     0,     0;
+        0,     0,     0,     0;
+        0,     0,     0,     0;
+        0,     0,     0,     0;
+1/(M + m),     0,     0,     0;
+        0, 1/Ixx,     0,     0;
+        0,     0, 1/Iyy,     0;
+        0,     0,     0, 1/Izz;
+        0,     0,     0,     0;
+        0,     0,     0,     0];
+
+% take parametry nutne rucne menit
+Ac = subs(Ac, [M Ixx Iyy Izz m d Ip g], [3.3 1.1 2.989 3.9 1 1 1*1^2 9.81]);
+Bc = subs(Bc, [M Ixx Iyy Izz m d Ip g], [3.3 1.1 2.989 3.9 1 1 1*1^2 9.81]);
+
+Ac = double(Ac); % a tohle uz muzeme analyzovat
+Bc = double(Bc);
+Cc = eye(8, 16); % na vystup pouze poloha, orientace a uhly zavazi
+% ale pro navrch stavove regulace potrebujeme upravenou matici C
+Cc_ = [
+    1     0     0     0     0     0     0     0     0     0     0     0     0     0     0     0;
+    0     1     0     0     0     0     0     0     0     0     0     0     0     0     0     0;
+    0     0     1     0     0     0     0     0     0     0     0     0     0     0     0     0;
+    0     0     0     0     0     1     0     0     0     0     0     0     0     0     0     0];
+
+Dc = 0; %
+
+ev = eig(Ac);
+
+% figure;
+% plot(real(ev), imag(ev), '*', 'MarkerFaceColor', 'b');
+% grid on;
+% xlabel('Real Part');
+% ylabel('Imaginary Part');
+% title('Eigenvalues with Imaginary Axis');
+% yline(0, '--k');
+% xline(0, '--k');
+% axis equal;
+% //ну это понятно, собственные числа//
+
+% Je system riditelny?
+R = ctrb(Ac, Bc);
+rank(R)
+
+% Je system pozorovatelny ?
+P = obsv(Ac, Cc_);
+rank(P)
+
+% Rozsireny stavovy popis -->
+% --> referencni x, y, z, yaw
+% --> +4 integracnich clenu v rozsirenem popisu
+Ac_ex = [Ac, zeros(16, 4); Cc_, zeros(4, 4)];
+Bc_ex = [Bc; zeros(4, 4)];
+
+syms x(t) y(t) z(t) phi(t) theta(t) psi(t) alpha(t) beta(t)
+syms Ix_ref Iy_ref Iz_ref Iyaw_ref
+q = transpose([x y z phi theta psi alpha beta]);
+q_dot = diff(q,1);
+X_ex = [q; q_dot; [Ix_ref;Iy_ref;Iz_ref;Iyaw_ref]];
+
+% Volba polu a vypocet koeficientu (16+4ref)
+% poles1 = [-1-1i;-1+1i;-2; -1-1.1i;-1+1.1i;-2.1; -0.5;-0.51;
+%          -3.1;-3.2;-3.3; -4.1;-4.2;-4.3; -0.66;-0.67;
+%          -1.7; -1.8; -1.9; -5]/10; % negunguji
+
+poles2 = [-4+3j;-4-3j; -3.5-3j;-3.5+3j; -3-2.5j;-3+2.5j;
+          -1.5-1j;-1.5+1j; -1-0.8j;-1+0.8j; -0.5-0.6j;-0.5+0.6j;
+          -3.5+2.5j;-3.5-2.5j; -3-2j;-3+2j;
+          -5; -4.5; -4.2; -3.8];
+
+
+K_ex_other_angles = place(Ac_ex, Bc_ex, poles2);
+ki_oa = K_ex_other_angles(:,17:20); % zesileni pro integracni cleny
+kp_oa = K_ex_other_angles(:, 1:16); % zesileni pro stavove cleny
+
