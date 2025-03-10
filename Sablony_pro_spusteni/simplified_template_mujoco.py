1b0e7b36fce135bcbccd41bde1864ffce1c99cf4
LevarGoldun <>
2025-03-10 16:02:01 +0100

    Adding new version of the model

--- Sablony_pro_spusteni/simplified_template_mujoco.py
+++ Sablony_pro_spusteni/simplified_template_mujoco.py
@@ -4,9 +4,11 @@
 import mujoco as mj
 import mujoco.viewer  # p.s. high-level, more user-friendly and abstract interface
 import time
+import numpy as np
 
-xml_path = 'model_quadcopter_v1.2_example.xml'  # xml file (assumes this is in the same folder as this file)
-simend = 10  # simulation time
+# xml_path = 'model_quadcopter_v1.2_example.xml'  # xml file (assumes this is in the same folder as this file)
+xml_path = 'model_quadcopter_v2.xml'
+simend = 300  # simulation time
 
 model = mj.MjModel.from_xml_path(xml_path)  # MuJoCo model
 data = mj.MjData(model)  # MuJoCo data
@@ -14,6 +16,21 @@ data = mj.MjData(model)  # MuJoCo data
 viewer = mujoco.viewer.launch_passive(model, data)
 
 start = time.time()
+
+# ======================================Data pro drobne deje v Matlab===================================================
+nv = len(data.qvel)
+M = np.zeros((nv, nv))
+mj.mj_fullM(model, M, data.qM)  # matice hmotnosti a setrvacnosti
+print("Matice hmotnosti a setrvacnosti:")
+print(M)
+
+# pocitam delku od motoru 4 do motoru 2 (vzdalenost 2*L dle Simulink)
+left_position = data.site('thrust1').xpos[1]
+right_position = data.site('thrust2').xpos[1]
+print("Polovicni delka kvadr.", abs(left_position-right_position)/2)
+print(left_position)
+# -> je symetricka s delkou 0.2051
+# ======================================================================================================================
 
 # Close the viewer automatically after simend wall-seconds.
 while viewer.is_running() and data.time < simend:
