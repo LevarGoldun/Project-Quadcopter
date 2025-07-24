16441bd8c6009891ca415fd0cd99ce1130efbea8
LevarGoldun <>
2025-07-24 17:10:16 +0200

    The correct latest version -correct b_moments and k_thrust -added A and Tau parameters of ZV -correct init values (Xinit) and singular values (Xs)

--- test_10_mat_equ_with_load/parametry_kopter_se_zavazim.m
+++ test_10_mat_equ_with_load/parametry_kopter_se_zavazim.m
@@ -1,7 +1,11 @@
 % 24.02.2025
 % 04.03.2025 - nove parametry pro v2
+
 % 14.03.2025 - byla udelana chyba pri urceni I kvadrokoptery. Mujoco pise
 % setrvacnost celeho systemu dron+zavazi. Samotny dron ma jine I, vic dole
+
+% 21.06.2025 - nove koeficienty k, b, nominalni otacky (predtim spatny
+% vypocet)
 
 % Parametry pro odvozeni_nelin_rovnic_INERTIA_LOAD.mlx a
 % kopter_se_zavazim_simulace.slx
@@ -25,28 +29,40 @@ Ip = m*d^2; %[kg*m2] setrvacnost zavazi (hm bod)
 
 g = 9.81;
 l = 0.2051; %[m] polovicni delka kvadrokoptery (rameno od hmotneho bodu)
-% как блять до этого для большего квадрокоптера было 0.086 м я хз...
 
-k_thrust = 2.3e-3; % koeficient umernosti pro generovani tahove sily
-% k = 0.1; % soucinitel odporu vzduchu
-b_moment = 5.4e-6; % koeficient umernosti odporoveho momentu vrtule
+% k_thrust = 2.3e-3; % koeficient umernosti pro generovani tahove sily
+k_thrust = 9.3e-6; %[N/(rad/s)^2]
 
+% k = 0.1; % soucinitel odporu vzduchu (uz neuvazujeme odpor)
+
+% b_moment = 5.4e-6; % koeficient umernosti odporoveho momentu vrtule
+b_moment = 3.6e-7;
+
+% hodnoty pro obycejny ZV shaper
+% A = 0.3919; % nespravna, ale tlumi
+A = 0.6081; % spravna, ale spatne tlumi (v DP tohle)
+T = 0.8682;
 %% Inicializace
+% pp poloha, orientace a vychyleni zavazi -> lze menit v blizkosti Xs, Us
 xyz_init = [0; 0; 0]; %pp poloha
 uhly_init = [0; 0; 0]; %pp orientace
-zavazi_init = [10*pi/180*0; 0]; %pp alpha a beta zavazi
+zavazi_init = [10*pi/180*0; 15*pi/180*0]; %pp alpha a beta zavazi
 
 xyz_dot_init = [0;0;0];
 uhly_dot_init = [0;0;0];
 zavazi_dot_init = [0;0];
 
 Xinit_matrix = [xyz_init; uhly_init; zavazi_init]; % pro maticovy tvar
-Xinit_ss = [Xinit_matrix; [0 0 0 0 0 0 0 0]'];
+Xinit_ss = [Xinit_matrix; [0 0 0 0 0 0 0 0]']; % pro ss tvar
+Uinit = [(M+m)*g; 0; 0; 0]; % pp na aktuatorech (vzdy jako Us)
 
-% singularni body pro linearni state-space se zavazim (pro vypocet delt)
-Xs_p = [0;0;0; 0;0;0; 0;0; 0;0;0; 0;0;0; 0;0];
-Us_p = [(M+m)*g; 0; 0; 0];
-% singularni bod pro vstupy ve tvaru otacek rotoru - jmenovite otacky pro
-% rovnovaznou polohu
-ms2 = (M+m)*g/(4*k_thrust); % je to hodnota otacky^2 !!!
-Ums_p = [ms2; ms2; ms2; ms2];
+% hodnoty singularniho bodu, v kterem byla provedena linearizace
+% (nemenime) -> pouziva se pro vypocet delt
+Xs = [0;0;0; 0;0;0; 0;0; 0;0;0; 0;0;0; 0;0];
+Us = [(M+m)*g; 0; 0; 0];
+% dX = X - Xs; dU = U - Us
+
+% singularni bod pro vstupy ve tvaru otacek rotoru - jmenovite uhlove rychlosti pro
+% rovnovaznou polohu (uhlove rychlosti, rad/s)
+w_square = (M+m)*g/(4*k_thrust); % je to hodnota (rad/s)^2 !!!
+Uws = [w_square; w_square; w_square; w_square];
